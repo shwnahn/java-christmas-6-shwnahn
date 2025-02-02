f0a0dadc932c9c38ce588c2661bdcc40f7ec1171
shwn <zxz8673@gmail.com>
2025-02-02 16:33:07 +0900

    test: 날짜입력 검증 테스트

---
+++ src/test/java/christmas/DateTest.java
@@ -0,0 +1,49 @@
+package christmas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DateTest {
+    String DateErrorMessage = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+
+    @Test
+    void 날짜_정상_입력_시_저장() {
+        // Given
+        int validDay = 15;
+        // When, Then
+        assertDoesNotThrow(() -> VisitDateValidator.validate(validDay));
+    }
+
+    @Test
+    void 날짜_최소값_미만_예외() {
+        // Given
+        int invalidDay = 0;
+        // When
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> VisitDateValidator.validate(invalidDay));
+        // Then
+        assertEquals(DateErrorMessage, exception.getMessage());
+    }
+
+    @Test
+    void 날짜_최대값_초과_예외() {
+        // Given
+        int invalidDay = 32;
+        // When
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> VisitDateValidator.validate(invalidDay));
+        // Then
+        assertEquals(DateErrorMessage, exception.getMessage());
+    }
+
+    void 날짜_숫자이외입력_예외() {
+        // Given
+        String invalidString = "abc";
+        Double invalidDouble = 12.3;
+        // When
+        Exception exceptionString = assertThrows(IllegalArgumentException.class, () -> VisitDateValidator.validate(invalidString));
+        Exception exceptionDouble = assertThrows(IllegalArgumentException.class, () -> VisitDateValidator.validate(invalidDouble));
+        // Then
+        assertEquals(DateErrorMessage, exceptionString.getMessage());
+        assertEquals(DateErrorMessage, exceptionDouble.getMessage());
+    }
+}
