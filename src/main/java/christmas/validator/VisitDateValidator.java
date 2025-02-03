9f74d26747853ab4338eb483328347cb8c64151f
shwn <zxz8673@gmail.com>
2025-02-03 10:04:26 +0900

    feat: 1. 식당 방문 날짜 지정

--- src/main/java/christmas/validator/VisitDateValidator.java
+++ src/main/java/christmas/validator/VisitDateValidator.java
@@ -1,13 +1,15 @@
-package christmas;
+package christmas.validator;
 
 import static christmas.ErrorMessage.ERROR_INVALID_DATE;
 
 public class VisitDateValidator {
-    public static void validate(Object day) {
-        if (!(day instanceof Integer)) {
+    public static void validate(String day) {
+        int dayInt;
+        try {
+            dayInt = Integer.parseInt(day);
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException(ERROR_INVALID_DATE);
         }
-        int dayInt = (Integer) day;
         if (dayInt < 1 || dayInt > 31) {
             throw new IllegalArgumentException(ERROR_INVALID_DATE);
         }
